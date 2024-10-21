4e535f86bf722b51248e15c29e59e52b4c7b6c1a
Arunprakaash <prakaasharun50@gmail.com>
2024-10-20 22:21:00 -0700

    fixed minor bugs

--- dist/src/background/service_worker.js
+++ dist/src/background/service_worker.js
@@ -1,18 +1,17 @@
 chrome.sidePanel
     .setPanelBehavior({ openPanelOnActionClick: true })
-    .catch(console.error);
+    .catch((error) => console.error(error));
 
-chrome.tabs.onActivated.addListener(({ tabId }) => extractPageContent(tabId));
-chrome.tabs.onUpdated.addListener((tabId) => extractPageContent(tabId));
+chrome.tabs.onActivated.addListener(({ tabId }) => { extractPageContent(tabId); });
+chrome.tabs.onUpdated.addListener(async (tabId) => { extractPageContent(tabId); });
 
 async function extractPageContent(tabId) {
     const tab = await chrome.tabs.get(tabId);
     if (!tab.url.startsWith('http')) return;
 
-    const [injection] = await chrome.scripting.executeScript({
+    const injection = await chrome.scripting.executeScript({
         target: { tabId },
         files: ['src/scripts/extract_content.js']
     });
-    console.log(injection.result);
-    chrome.storage.session.set({ pageContent: injection.result });
+    chrome.storage.session.set({ pageContent: injection[0].result });
 }
